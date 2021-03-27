7a7dc8f7116d3f8293004a82b57de76f724602d0
Iago Lobato <hynn@live.jp>
2021-03-27 19:47:56 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,19 +12,12 @@ export default function Home() {
 			<main className={styles.main}>
 				<div className={styles.container}>
 					<h1>Seja bem vindo, <strong>Visitante</strong>!</h1>
-					<img style="max-width: 300px;" src="ilobatodev_astronauta1"></img>
+					<img style={{maxWidth: 300 + 'px'}} src="ilobatodev_astronauta1"/>
 				</div>
 			</main>
 
 			<footer className={styles.footer}>
-				<a
-					href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Powered by{' '}
-					<img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-				</a>
+					Powered by{' - '}Hynn
 			</footer>
 		</div>
 	)
