9ae8b478e682157b7ea85f3cde27c92cc9077398
Iago Lobato <hynn@live.jp>
2021-10-16 22:31:04 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,99 +2,146 @@ import Head from 'next/head';
 import Trabalhos from '../pages/trabalhos.js';
 import SobreMim from '../pages/sobremim.js';
 import Contato from '../pages/contato.js';
-// import { Scrollbars } from 'react-custom-scrollbars-2';
-
-// const checkClick = (event) => {
-// 	let nameFromButton = event.target.dataset.value;
-// 	let checkForDivsOpened = document.querySelectorAll('div.secondPanel.d-flex');
-
-// 	if(checkForDivsOpened.length > 0) {
-// 		checkForDivsOpened.forEach(item => {
-// 			if(!item.id.includes(nameFromButton)) {
-// 				item.classList.toggle('d-none');
-// 				item.classList.toggle('d-flex');
-// 			}
-// 		})
-// 	}
-
-// 	let divToChange = document.getElementById(nameFromButton);
-// 	divToChange.classList.toggle('d-none');
-// 	divToChange.classList.toggle('d-flex');
-// }
 
 export default function Home() {
 	return (
-		<main>
-			<Head>
-				<meta charSet="utf-8"/>
-				<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no"/>
-				<title>ILobato Dev</title>
-				<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.0-beta3/css/bootstrap.min.css"/>
-				<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Oswald:200,400"/>
-				<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins"/>
-			</Head>
+	<main>
+		<Head>
+			<meta charset="utf-8"/>
+			<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no"/>
+			<title>PortfolioTry1</title>
+			<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"/>
+			<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,600,700,800,900&amp;display=swap"/>
+			<link rel="stylesheet" href="assets/css/styles.min.css?h=5da7be15da27bf315b47f1084262bea0"/>
+		</Head>
 
-			<section>
-				<div className="container">
-					<div className="row">
-						<div className="col-sm-4 d-flex flex-column justify-content-center align-items-center mx-auto">
-							<div className="d-flex flex-column justify-content-center align-items-center vh-100">
-								<img className="mb-3 bigBoy" src="assets/img/ilobatodev_astronauta2.svg?h=6dde2b2d3ce2d634c31eef9f6ff1ecad"/>
-									<h1 className="logo">&lt;IL.<span>DEV</span>/&gt;</h1>
-									<h2 className="subTitle mb-4 text-center">WEBDEVELOPER</h2>
-									<a className="btn btn-personalizado w-100 mb-3" href="#" data-value="trabalhos" data-bs-toggle="modal" data-bs-target="#trabalhos">TRABALHOS</a>
-									<a className="btn btn-personalizado w-100 mb-3" href="#" data-value="sobreMim" data-bs-toggle="modal" data-bs-target="#sobremim">SOBRE MIM</a>
-									<a className="btn btn-personalizado w-100 mb-4" href="#" data-value="contato" data-bs-toggle="modal" data-bs-target="#contato">CONTATO</a>
-									<p className="footerCredits text-center">Todos os direitos reservados ©</p>
+		<section class="py-5 my-5">
+			<div class="container my-auto">
+				<div class="row">
+					<div class="col-3 d-xl-flex flex-column justify-content-xl-center align-items-xl-center px-4"><img src="assets/img/ilobatodev_astronauta2.svg?h=bdd18d63a557c289979440babb853d29" style="width: 150px;">
+						<h1 class="mt-1 px-4 text-center" style="font-size: 25px;border-bottom: 1px solid #ccc;padding-bottom: .5em;margin-bottom: .5em;">Iago Lobato</h1>
+						<p class="fw-bold" style="font-size: 13px;">Estatísticas</p>
+						<div class="d-xl-flex align-items-xl-center justify-content-between d-inline-flex w-100 statusItem">
+							<p class="mb-0 fw-bold" style="font-size: 13px;">Trabalhos concluídos</p>
+							<p class="mb-0 fw-bold">1</p>
+						</div>
+						<div class="d-xl-flex align-items-xl-center justify-content-between d-inline-flex w-100 statusItem">
+							<p class="mb-0 fw-bold" style="font-size: 13px;">Trabalhos concluídos</p>
+							<p class="mb-0 fw-bold">1</p>
+						</div>
+						<div class="d-xl-flex align-items-xl-center justify-content-between d-inline-flex w-100 statusItem">
+							<p class="mb-0 fw-bold" style="font-size: 13px;">Trabalhos concluídos</p>
+							<p class="mb-0 fw-bold">1</p>
+						</div>
+						<div class="d-xl-flex align-items-xl-center justify-content-between d-inline-flex w-100 statusItem">
+							<p class="mb-0 fw-bold" style="font-size: 13px;">Trabalhos concluídos</p>
+							<p class="mb-0 fw-bold">1</p>
+						</div>
+					</div>
+					<div class="col">
+						<h1 class="mb-3 fw-bolder" style="font-size: 37px;">Dashboard</h1>
+						<div class="d-flex p-3" style="background-color: #fff;border-radius: 5px;box-shadow: 0 10px 10px 0 rgba(0,0,0,0.05);">
+							<div class="col px-2">
+								<h1 class="fw-bold" style="font-size: 16px;">Todos os trabalhos</h1>
+								<div class="listItem" style="display: flex;">
+									<div class="me-2">
+										<div class="roundImage"></div>
+									</div>
+									<div class="d-xl-flex flex-column justify-content-xl-start align-items-xl-center">
+										<p class="mb-0 fw-bold" style="font-size: 13px;">Trabalho X</p>
+										<p class="mb-0" style="font-size: 13px;">Trabalho X</p>
+									</div>
+								</div>
+								<div class="listItem" style="display: flex;">
+									<div class="me-2">
+										<div class="roundImage"></div>
+									</div>
+									<div class="d-xl-flex flex-column justify-content-xl-start align-items-xl-center">
+										<p class="mb-0 fw-bold" style="font-size: 13px;">Trabalho X</p>
+										<p class="mb-0" style="font-size: 13px;">Trabalho X</p>
+									</div>
+								</div>
+								<div class="listItem" style="display: flex;">
+									<div class="me-2">
+										<div class="roundImage"></div>
+									</div>
+									<div class="d-xl-flex flex-column justify-content-xl-start align-items-xl-center">
+										<p class="mb-0 fw-bold" style="font-size: 13px;">Trabalho X</p>
+										<p class="mb-0" style="font-size: 13px;">Trabalho X</p>
+									</div>
+								</div>
+								<div class="listItem" style="display: flex;">
+									<div class="me-2">
+										<div class="roundImage"></div>
+									</div>
+									<div class="d-xl-flex flex-column justify-content-xl-start align-items-xl-center">
+										<p class="mb-0 fw-bold" style="font-size: 13px;">Trabalho X</p>
+										<p class="mb-0" style="font-size: 13px;">Trabalho X</p>
+									</div>
+								</div>
+								<div class="listItem" style="display: flex;">
+									<div class="me-2">
+										<div class="roundImage"></div>
+									</div>
+									<div class="d-xl-flex flex-column justify-content-xl-start align-items-xl-center">
+										<p class="mb-0 fw-bold" style="font-size: 13px;">Trabalho X</p>
+										<p class="mb-0" style="font-size: 13px;">Trabalho X</p>
+									</div>
+								</div>
+							</div>
+							<div class="col px-1">
+								<h1 class="fw-bold" style="font-size: 16px;">Todos os trabalhos</h1>
+								<div class="listItem" style="display: flex;">
+									<div class="me-2">
+										<div class="roundImage"></div>
+									</div>
+									<div class="d-xl-flex flex-column justify-content-xl-start align-items-xl-center">
+										<p class="mb-0 fw-bold" style="font-size: 13px;">Trabalho X</p>
+										<p class="mb-0" style="font-size: 13px;">Trabalho X</p>
+									</div>
+								</div>
+								<div class="listItem" style="display: flex;">
+									<div class="me-2">
+										<div class="roundImage"></div>
+									</div>
+									<div class="d-xl-flex flex-column justify-content-xl-start align-items-xl-center">
+										<p class="mb-0 fw-bold" style="font-size: 13px;">Trabalho X</p>
+										<p class="mb-0" style="font-size: 13px;">Trabalho X</p>
+									</div>
+								</div>
+								<div class="listItem" style="display: flex;">
+									<div class="me-2">
+										<div class="roundImage"></div>
+									</div>
+									<div class="d-xl-flex flex-column justify-content-xl-start align-items-xl-center">
+										<p class="mb-0 fw-bold" style="font-size: 13px;">Trabalho X</p>
+										<p class="mb-0" style="font-size: 13px;">Trabalho X</p>
+									</div>
+								</div>
+								<div class="listItem" style="display: flex;">
+									<div class="me-2">
+										<div class="roundImage"></div>
+									</div>
+									<div class="d-xl-flex flex-column justify-content-xl-start align-items-xl-center">
+										<p class="mb-0 fw-bold" style="font-size: 13px;">Trabalho X</p>
+										<p class="mb-0" style="font-size: 13px;">Trabalho X</p>
+									</div>
+								</div>
+								<div class="listItem" style="display: flex;">
+									<div class="me-2">
+										<div class="roundImage"></div>
+									</div>
+									<div class="d-xl-flex flex-column justify-content-xl-start align-items-xl-center">
+										<p class="mb-0 fw-bold" style="font-size: 13px;">Trabalho X</p>
+										<p class="mb-0" style="font-size: 13px;">Trabalho X</p>
+									</div>
+								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-			</section>
-
-			<div className="modal fade" id="trabalhos" tabIndex="-1" aria-labelledby="trabalhos" aria-hidden="true">
-				<div className="modal-dialog modal-dialog-centered modal-dialog-scrollable">
-					<div className="modal-content">
-						<button type="button" className="btn-close2" data-bs-dismiss="modal" aria-label="Close">
-							<i class="fas fa-times"></i>
-						</button>
-
-						<div className="modal-body">
-							<Trabalhos />
-						</div>
-					</div>
-				</div>
 			</div>
-
-			<div className="modal fade" id="sobremim" tabIndex="-1" aria-labelledby="sobremim" aria-hidden="true">
-				<div className="modal-dialog modal-dialog-centered modal-dialog-scrollable">
-					<div className="modal-content">
-						<button type="button" className="btn-close2" data-bs-dismiss="modal" aria-label="Close">
-							<i class="fas fa-times"></i>
-						</button>
-
-						<div className="modal-body">
-							<SobreMim />
-						</div>
-					</div>
-				</div>
-			</div>
-
-			<div className="modal fade" id="contato" tabIndex="-1" aria-labelledby="contato" aria-hidden="true">
-				<div className="modal-dialog modal-dialog-centered modal-dialog-scrollable">
-					<div className="modal-content">
-						<button type="button" className="btn-close2" data-bs-dismiss="modal" aria-label="Close">
-							<i class="fas fa-times"></i>
-						</button>
-
-						<div className="modal-body">
-							<Contato />
-						</div>
-					</div>
-				</div>
-			</div>
-
-			<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.0-beta3/js/bootstrap.bundle.min.js"></script>
-		</main>
+		</section>
+	</main>
 	)
 }
